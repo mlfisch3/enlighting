3c73d2e2e2a6d885efc154e874a0a1db1303d6fa
mlfisch3 <38837568+mlfisch3@users.noreply.github.com>
2022-10-29 02:15:34 -0400

    theme update in setup.sh

--- setup.sh
+++ setup.sh
@@ -1,10 +1,18 @@
 mkdir -p ~/.streamlit/
 
-
 echo "\
+[runner]\n\
+fastReruns = false\n\
 [server]\n\
 headless = true\n\
 port = $PORT\n\
 enableCORS = false\n\
+enableXsrfProtection=false\n\
+[theme]\n\
+primaryColor="#dca5ce"\n\
+backgroundColor="#0e1117"\n\
+secondaryBackgroundColor="#31333f"\n\
+textColor="#fafafa"\n\
+font="sans serif"\n\
 \n\
 " > ~/.streamlit/config.toml
